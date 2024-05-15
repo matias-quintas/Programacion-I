80c664ad40d481e6c6e771af05354fb5c68ea8fb
Maty <matyquintas@gmail.com>
2024-05-15 20:47:42 -0300

    second commit

--- 2024.05.15 - Ciclos/clase.c
+++ 2024.05.15 - Ciclos/clase.c
@@ -1,1 +1,27 @@
 // unsigned int → devuelve numeros enteros positivos unicamente, el signed es como decir int solo
+
+// Ejercitacion Level 1
+
+#include <stdio.h>
+#define p printf
+#define s scanf
+
+int main(void)
+{
+  uno();
+  dos();
+  tres();
+  return 0;
+}
+
+int uno(){
+  return 0;
+}
+
+int dos(){
+  return 0;
+}
+
+int tres(){
+  return 0;
+}
