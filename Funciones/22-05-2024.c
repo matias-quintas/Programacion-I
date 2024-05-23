7e2260d2c4a666d5b50711e9588d743c87434f3f
Maty <matyquintas@gmail.com>
2024-05-22 21:36:20 -0300

    Final

---
+++ Funciones/22-05-2024.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define p printf
+#define s scanf
+
+int main(void /*Aca adentro van los argumentos*/)
+{
+    return 0;
+}
+
+// Prototipos:
+
+int suma (int,int); // La funcion devuelve un entero pero recibe 2 argumentos enteros.
