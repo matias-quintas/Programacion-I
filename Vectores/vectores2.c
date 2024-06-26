c76aa40245ab40fffee257144784b7a5a23e8535
Maty <matyquintas@gmail.com>
2024-06-26 20:39:10 -0300

    26-feb-2024

--- Vectores/vectores2.c
+++ Vectores/vectores2.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <time.h>
 #define TAMANO_VECTOR 5
 
 int main()
 {
-    int vector[TAMANO_VECTOR]={23,67,54,6,45};
+    float vector[TAMANO_VECTOR]={23,67,54,6,45};
     int i;
-
+    srand(time(NULL));
+    // Inicializar el vector con valores
+    for (i = 0; i < TAMANO_VECTOR; i++)
+    {
+        vector[i] = rand()%(100-20+1)-20;
+    }
+    
+    //Imprimir el vector
+    printf("\nValores del Vector:\n\n ");
+    for (i = 0; i < TAMANO_VECTOR; i++)
+    {
+        printf("vector[%d]=%.2f\n ", i, vector[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
 
